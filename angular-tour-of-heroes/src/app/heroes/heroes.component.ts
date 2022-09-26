0726b721e243dd8f632aa464dab2001428ba9b3a
Johnathan <jonathanumana86@gmail.com>
2022-09-26 16:14:07 -0400

    Added detail view & user can see heros details

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -8,16 +8,21 @@ import { HEROES } from './mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
 
+  selectedHero?: Hero
   heros = HEROES
+
+
   constructor() { }
 
   ngOnInit(): void {
     console.log(this.heros);
+  }
+
+  onSelect(hero: Hero){
+    this.selectedHero = hero
+    console.log(this.selectedHero);
+
   }
 
 }
