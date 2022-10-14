4f56200fa4c3b957567ac5a12ec74152e5c19057
Johnathan <jonathanumana86@gmail.com>
2022-10-14 01:01:11 -0400

    Adding httpclient & using tap & pipe

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -4,6 +4,7 @@ import { HEROES } from './heroes/mock-heroes';
 import { Observable, of } from 'rxjs'
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,10 @@ export class HeroService {
 /** GET heroes from the server */
 getHeroes(): Observable<Hero[]> {
   return this.http.get<Hero[]>(this.heroesUrl)
+  .pipe(
+    tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
 }
 
   getHero(id: number): Observable<Hero> {
@@ -32,6 +37,27 @@ getHeroes(): Observable<Hero[]> {
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ *
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
 }
 
 
